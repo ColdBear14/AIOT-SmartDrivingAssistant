3737b22c0790dafcdfa8f432b7d49e0e74b2335e
Khoawawa <atom08072004@gmail.com>
2025-03-10 09:52:28 +0700

    a

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,37 @@
 from pymongo import MongoClient
 import requests
+import json
 from datetime import datetime
-from backend.utils import Config
+from utils import Config
 # AdaFruit Credential
 config = Config()
 #MongoDB setup
 class Database():
-  def __init__(self):
+  def __init__(self,db_name = config.db_name):
     self.client = MongoClient(config.mongo_url)
-    self.db = self.client['va_database']
-    self.collections = ['lux','temp','humid']
+    self.db = self.client[db_name]
+    self.collections = set(self.db.list_collection_names())
+  def ensure_collection(func):
+    def wrapper(self, collection_name, document):
+      if collection_name not in self.db.list_collection_names():
+          self.db.create_collection(
+              collection_name,
+              timeseries={"timeField": "timestamp", "granularity": "seconds"}
+          )
+          print(f"Collection '{collection_name}' created successfully.")
+      return func(self, collection_name, document)
+    return wrapper  
+  @ensure_collection
   def push_to_db(self,collection_name,document):
+    document['timestamp'] = datetime.now()
     self.db[collection_name].insert_one(document)
     print(f'Store in MongoDB: {document}')
+
+
+if __name__ == '__main__':
+  db = Database('test')
+  document = {
+    'a': 'a',
+    'b': 'b'
+  }
+  db.push_to_db('collection',document)
--- backend/utils.py
+++ backend/utils.py
@@ -3,11 +3,14 @@ AIO_USER_NAME = "NopeHy14"
 AIO_KEY = "aio_RfGU73f5BHFvSvJIfnRNRujgbzBm"
 @dataclass
 class Config:
-    mongo_url: str = "mongodb://local_host:27017"
+    mongo_url: str = "mongodb://localhost:27017"
     aio_user: str = AIO_USER_NAME
     aio_key: str = AIO_KEY
     aio_url: str = field(init=False)
+    db_name: str = 'va_database'
+    collection: list[str] = ['temp','humid','lux']
     def __post_init__(self):
         self.aio_url = f'https://io.adafruit.com/{self.aio_user}/feeds/'
     
 config = Config()
+
