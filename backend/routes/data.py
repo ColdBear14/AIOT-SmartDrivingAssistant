4b997057f219f9158a515d7b0da7e0c4056821af
Khoawawa <atom08072004@gmail.com>
2025-03-15 23:25:05 +0700

    a

--- backend/routes/data.py
+++ backend/routes/data.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter, Depends
-from utils import get_collection
+from fastapi import APIRouter, Depends, Request
+from utils import get_collection, get_uid
 from models import SensorRequest
 router = APIRouter()
 
 def get_sensor_collection():
     return get_collection('environment_sensor')
+def get_user_collection():
+    return get_collection('user')
+
 @router.post('/sensor_data')
-async def get_sensor_data(sensor:SensorRequest,sensors_collection=Depends(get_sensor_collection)):
-    sensors_collection.find()
+async def get_sensor_data(sensor:SensorRequest,request: Request,sensors_collection=Depends(get_sensor_collection)):
+    uid = get_uid(request=request)
+    
+    sensors_collection.find({'uid': uid})
